802e0debfa7ca2304ccc4c957a6964579b20ded6
Grand Ed <eduloko12@gmail.com>
2022-07-15 18:30:31 -0300

    Update

---
+++ Assets/player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public GameObject ObjectToAttach;
+    public float sensitivity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private GameObject CurrentObjectAttach;
+    public float levitationvalue;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray TryHit = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit TheHit;
+            if (Physics.Raycast(TryHit, out TheHit))
+            {
+                if (TheHit.transform.CompareTag("Player"))
+                {
+                    CurrentObjectAttach = Instantiate(ObjectToAttach, TheHit.point, Quaternion.identity);
+                    CurrentObjectAttach.GetComponent<ConfigurableJoint>().connectedBody = TheHit.rigidbody;
+                }
+            }
+        }
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(CurrentObjectAttach);
+        }
+
+        if (CurrentObjectAttach != null)
+        {
+            Vector3 GrabPosition = CurrentObjectAttach.transform.position;
+            CurrentObjectAttach.transform.position = new Vector3(GrabPosition.x + Input.GetAxis("Mouse X") * sensitivity, Mathf.Lerp(GrabPosition.y, levitationvalue, Time.deltaTime), GrabPosition.z + Input.GetAxis("Mouse Y") * sensitivity);
+        }
+    }
+}
